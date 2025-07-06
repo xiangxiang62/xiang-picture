96107c0facfb1b40b28565067889fd50b99a137a
xiangxiang <1964362087@qq.com>
2025-07-06 16:13:43 +0800

    feat: 后端项目初始化

---
+++ xiang-picture/xiang-picture-backend/src/main/java/com/xiang/xiangpicturebackend/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.xiang.xiangpicturebackend.controller;
+
+import com.xiang.xiangpicturebackend.common.BaseResponse;
+import com.xiang.xiangpicturebackend.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
